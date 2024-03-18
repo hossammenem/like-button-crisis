71b1a665ae0ae616db4d8a84e1c4069a26caf848
hossammenem <hossammenem0102840@gmail.com>
2024-03-19 00:49:36 +0200

    not sure what happened here, it's been a long time

--- src/QueryPref.js
+++ src/QueryPref.js
@@ -1,24 +1,24 @@
 const queries = [
   {
-    'queryStr': 'SELECT * FROM tabluno where user_name = $1',
-    'options': ['08544f75d007ae439925bb21fe48c64c'],
+    queryStr: "SELECT * FROM tabluno where user_name = $1",
+    options: ["08544f75d007ae439925bb21fe48c64c"],
   },
   {
-    'queryStr': 'SELECT * FROM tabluno2 where user_name = $1',
-    'options': ['f67a10e60fde706bb6c5948374b385e5'],
+    queryStr: "SELECT * FROM tabluno2 where user_name = $1",
+    options: ["f67a10e60fde706bb6c5948374b385e5"],
   },
-]
-const maxIters = 5000; 
-const progressInterval = Math.floor(maxIters / 10); 
+];
+const maxIters = 5000;
+const progressInterval = Math.floor(maxIters / 10);
 
 async function queryPref(client) {
-  for(const query of queries) {
-    console.log('\n' + query.queryStr);
-    console.log('-----------------------------------------');
+  for (const query of queries) {
+    console.log("\n" + query.queryStr);
+    console.log("-----------------------------------------");
     let avg = 0;
-    for(let i = 1; i <= maxIters; i++)    {
+    for (let i = 1; i <= maxIters; i++) {
       const start = performance.now();
-      await client.query(query.queryStr, query.options)
+      await client.query(query.queryStr, query.options);
       const end = performance.now();
       avg += end - start;
 
@@ -31,6 +31,5 @@ async function queryPref(client) {
     }
   }
 }
-
 
 module.exports = queryPref;
--- src/feed.js
+++ src/feed.js
@@ -1,24 +1,31 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const args = process.argv.slice(2);
 
-const rows = +args[args.indexOf('-c')+1];
-const table = args.indexOf('-t') != -1 ? args[args.indexOf('-t')+1] : 'liked_posts';
+const rows = +args[args.indexOf("-c") + 1];
+const table =
+  args.indexOf("-t") != -1 ? args[args.indexOf("-t") + 1] : "liked_posts";
 
 const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: args.indexOf('-db') != -1 ? args[args.indexOf('-db')+1] : 'indexing-is-really-hard',
+  user: "postgres",
+  host: "localhost",
+  database:
+    args.indexOf("-db") != -1
+      ? args[args.indexOf("-db") + 1]
+      : "indexing-is-really-hard",
   password: process.env.DB_PASSWORD,
-  port: 5432
+  port: 5432,
 });
 
-(async ()=> {
-await client.connect();
+(async () => {
+  await client.connect();
 
-await client.query(`INSERT INTO ${table} SELECT uuid_generate_v4(), uuid_generate_v4(), CURRENT_DATE FROM generate_series(1,$1);`, [rows]);
+  await client.query(
+    `INSERT INTO ${table} SELECT uuid_generate_v4(), uuid_generate_v4(), CURRENT_DATE FROM generate_series(1,$1);`,
+    [rows],
+  );
 
-client.end();
+  client.end();
 
-console.log('DONE!');
+  console.log("DONE!");
 })();
